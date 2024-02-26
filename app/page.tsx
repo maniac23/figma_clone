419949edae8581136ce12f5845f59ab759ba488a
maniac23 <tukaym@gmail.com>
2024-02-26 17:41:00 +0300

    Dimension changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,14 @@ import {
   handleCanvasMouseDown,
   handleCanvasMouseUp,
   handleCanvasObjectModified,
+  handleCanvasObjectScaling,
+  handleCanvasSelectionCreated,
   handleCanvaseMouseMove,
   handleResize,
   initializeFabric,
   renderCanvas,
 } from '@/lib/canvas';
-import { ActiveElement } from '@/types/type';
+import { ActiveElement, Attributes } from '@/types/type';
 import { useMutation, useRedo, useStorage, useUndo } from '@/liveblocks.config';
 import { root } from 'postcss';
 import { defaultNavElement } from '@/constants';
@@ -32,6 +34,7 @@ export default function Page() {
   const selectedShapeRef = useRef<string | null>(null);
   const activeObjectRef = useRef<fabric.Object | null>(null);
   const imageInputRef = useRef<HTMLInputElement>(null);
+  const isEditingRef = useRef(false);
 
   const canvasObjects = useStorage((root) => root.canvasObjects);
 
@@ -48,6 +51,16 @@ export default function Page() {
     name: '',
     value: '',
     icon: '',
+  });
+
+  const [elementAttributes, setElementAttributes] = useState<Attributes>({
+    width: '',
+    height: '',
+    fontSize: '',
+    fontFamily: '',
+    fontWeight: '',
+    fill: '#aabbcc',
+    stroke: '#aabbcc',
   });
 
   const deleteAllShapes = useMutation(({ storage }) => {
@@ -119,6 +132,14 @@ export default function Page() {
       handleCanvasObjectModified({ options, syncShapeInStorage });
     });
 
+    canvas.on('selection:created', (options) => {
+      handleCanvasSelectionCreated({ options, isEditingRef, setElementAttributes });
+    });
+
+    canvas.on('object:scaling', (options) => {
+      handleCanvasObjectScaling({ options, setElementAttributes });
+    });
+
     window.addEventListener('resize', () => {
       handleResize({ canvas }); // fabricRef
     });
@@ -166,7 +187,14 @@ export default function Page() {
       <section className="flex h-full flex-row">
         <LeftSidebar allShapes={Array.from(canvasObjects)} />
         <Live canvasRef={canvasRef} />
-        <RightSidebar />
+        <RightSidebar
+          elementAttributes={elementAttributes}
+          setElementAttributes={setElementAttributes}
+          fabricRef={fabricRef}
+          isEditingRef={isEditingRef}
+          activeObjectRef={activeObjectRef}
+          syncShapeInStorage={syncShapeInStorage}
+        />
       </section>
     </main>
   );
