df706a8b30e3095039335045c2dc5d489789c37a
maniac23 <tukaym@gmail.com>
2024-02-26 18:40:06 +0300

    Comments

--- components/Live.tsx
+++ components/Live.tsx
@@ -6,6 +6,7 @@ import { CursorMode, CursorState, Reaction, ReactionEvent } from '@/types/type';
 import ReactionSelector from './reaction/ReactionButton';
 import FlyingReaction from './reaction/FlyingReaction';
 import useInterval from '@/hooks/useInterval';
+import { Comments } from './comments/Comments';
 
 type Props = {
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
@@ -144,7 +145,7 @@ const Live = ({ canvasRef }: Props) => {
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
-      className="h-[100vh] w-full flex justify-center items-center text-center">
+      className="relative h-full w-full flex flex-1 justify-center items-center">
       <canvas ref={canvasRef} />
       {reactions.map((r) => (
         <FlyingReaction
@@ -167,6 +168,7 @@ const Live = ({ canvasRef }: Props) => {
         <ReactionSelector setReaction={setReaction} />
       )}
       <LiveCursors others={others} />
+      <Comments />
     </div>
   );
 };
